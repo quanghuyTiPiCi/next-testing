de3448694f2c9258f9b77d31f9cd00a4c5b8eb9b
quanghuyTiPiCi <quanghuy.tipici@gmail.com>
2022-12-16 21:07:32 +0700

    next commit

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,27 @@
-import { useRouter } from 'next/router';
 import BackgroundHacker from '../components/BackgroundHacker';
 import Caption from '../components/Caption';
 import CardItem from '../components/CardItem';
 import CardProfile from '../components/CardProfile';
 import CardWater from '../components/CardWater';
-import HeaderItem from '../components/HeaderItem';
 import ItemGlass from '../components/ItemGlass';
 import LoadingCustomize from '../components/Loading';
 import LogoSlider from '../components/LogoSlider';
-import CitiesSlider from '../components/Slide';
+import { Facebook } from '../facebook';
 
 export default function Home() {
-  const router = useRouter();
-
-  const handleLanguage = (l) => {
-    router.push('/', undefined, { locale: l });
-  };
   return (
     <div
     // style={{ display: 'flex', flexDirection: 'column', gap: '0px 0' }}
     >
-      <p>{router.locale}</p>
-
-      {/* <CardWater />
+      <CardWater />
       <CardItem />
-      <CardProfile /> */}
-      {/* <LoadingCustomize size={7} /> */}
-      {/* <LogoSlider/> */}
-      {/* <BackgroundHacker /> */}
-      {/* <Caption /> */}
-      {/* <HeaderItem /> */}
-      {/* <ItemGlass /> */}
-      {/* <CitiesSlider /> */}
+      <CardProfile />
+      <LoadingCustomize size={7} />
+      <LogoSlider />
+      <BackgroundHacker />
+      <Caption />
+      <ItemGlass />
+      <Facebook />
     </div>
   );
 }
